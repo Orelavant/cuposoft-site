94d4d254ede61d7bcf3b499f3868aad90f7e06f7
Orelavant <jacobkvalero@gmail.com>
2025-03-26 20:51:53 -0400

    refactor

--- sketch.js
+++ sketch.js
@@ -1,38 +1,71 @@
-function setup() {
-  //creates a canvas 600 pixels wide
-  //and 400 pixels high
+let numberOfDots;
+let diameter = 8;
+let accel = 0.01;
+let amplitude = 40;
+let dotDivisor = 50
+let heightDivisor = 2;
+let height;
+let dotArr;
+
+function init() {
   createCanvas(windowWidth, windowHeight);
+  height = windowHeight / heightDivisor;
+  numberOfDots = floor(windowWidth / dotDivisor);
+  dotArr = createDotArr(numberOfDots, height, diameter);
 }
 
 function draw() {
   background(255, 246, 211);
 
-  // params
-  let numberOfDots = 100;
-  let height = windowHeight / 2;
-  let diameter = 5;
-  let accel = 0.01;
-  let amplitude = 40;
-  textSize(32);
+  dots(dotArr);
+  wave(dotArr, accel, amplitude);
+}
 
-  // draw dots
+function createDotArr(numberOfDots, height, diameter) {
+  let dotArr = [];
+
   for (let i = 0; i <= numberOfDots; i++) {
-    // point 1 for line
     let x = windowWidth / numberOfDots * i;
-    let sinValue = sin((frameCount + x) * accel) * amplitude;
+    dotArr.push({"x" : x, "h" : height, "d" : diameter});
+  }
+
+  return dotArr;
+}
+
+function dots(dotArr) {
+  for (let dot of dotArr) {
+    fill("white");
+    strokeWeight(2);
+    circle(dot.x, dot.h, dot.d);
+  }
+}
+
+function wave(dotArr, accel, amplitude) {
+  for (let i = 0; i < dotArr.length - 1; i++) {
+    let dot = dotArr[i];
+
+    // point 1 for line
+    let sinValue = sin((frameCount + dot.x) * accel) * amplitude;
 
     // point 2 for line
     let x1 = windowWidth / numberOfDots * (i + 1);
     let sinValue1 = sin((frameCount + x1) * accel) * amplitude;
 
-    // draw wave
     // draw dots
     fill("black");
-    strokeWeight(3);
-    circle(x, height + sinValue, diameter);
+    strokeWeight(2);
+    circle(dot.x, dot.h + sinValue, dot.d);
 
     // draw line
     // strokeWeight(6);
     // line(x, height + sinValue, x1, height + sinValue1);
   }
+}
+
+function setup() {
+  init();
+}
+
+function windowResized() {
+  init();
 }
