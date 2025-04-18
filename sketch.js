8bc9059fbbe62d17b939296be05377811883a1f1
unknown <jacobkvalero@gmail.com>
2025-04-18 15:22:15 -0400

    waving impl commented out

--- sketch.js
+++ sketch.js
@@ -12,6 +12,8 @@ let boatEndOffset = (boatWidth / 2) + 15;
 let boatX = -boatStartOffset;
 let boatMinPitch = -0.55;
 let boatMaxPitch = 0.55;
+let flagNoiseXOff;
+let flagNoiseYOff;
 let height;
 let dotArr;
 
@@ -32,6 +34,8 @@ function init() {
   height = windowHeight / heightDivisor;
   numberOfDots = floor(windowWidth / dotDivisor);
   dotArr = createDotArr(numberOfDots, height, diameter);
+  flagNoiseXOff = random(10000);
+  flagNoiseYOff = random(10000);
 }
 
 function draw() {
@@ -47,6 +51,10 @@ function draw() {
 
   wave(dotArr, waveAccel, amplitude);
   boat(boatX, height, boatWidth, boatHeight, waveAccel, amplitude);
+  push();
+    scale(0.5);
+    boat(boatX, height, boatWidth, boatHeight, waveAccel, amplitude);
+  pop();
 }
 
 function boat(x, y, width, height, waveAccel, amplitude) { 
@@ -55,6 +63,8 @@ function boat(x, y, width, height, waveAccel, amplitude) {
   let localHeight = -50;
   let sinRotate = sin(((frameCount + x) * waveAccel) + PI / 2);
   let sinRotateNorm = map(sinRotate, -1, 1, boatMinPitch, boatMaxPitch);
+  let flagXOff = map(noise(flagNoiseXOff), 0, 1, -3, 3); 
+  let flagYOff = map(noise(flagNoiseYOff), 0, 1, -3, 3);
   if (sinRotateNorm < 0) {
     boatSpeed = max(0.4, boatSpeed - waveAccel * 1.4);
   } else if (sinRotateNorm > 0) {  
@@ -89,16 +99,24 @@ function boat(x, y, width, height, waveAccel, amplitude) {
 
     // Draw the flag
     translate(width / 2, -80);
+    flagNoiseXOff += 0.01;
+    flagNoiseYOff += 0.01;
     fill(255, 246, 211);
+
+    // Waving implementation
+    // Back flag
+    // line(-35 + flagXOff, -20 + flagYOff, 0, -25);
+    // Front flag
+    // triangle(0, 0, 0, -25, -35 + flagXOff, -15 + flagYOff);
+    // Connecting line
+    // line(-35 + flagXOff, -20 + flagYOff, -36 + flagXOff, -15 + flagYOff);
+
     // Back flag
     line(-35, -20, 0, -25);
     // Front flag
-    triangle(0, 0, 0, -25, -35, -15);
+    triangle(0, 0, 0, -25, -35,  -15);
     // Connecting line
-    line(-35, -20, -35, -15);
-
-    // Attempt at waving flag
-    // triangle(0, 0, 0, -25, -35 + map(noise(frameCount * 0.015), 0, 1, -20, 20), -15 + map(noise(frameCount * 0.015), 0, 1, -20, 20));
+    line(-35, -20, -36, -15);
   pop();
 }
 
